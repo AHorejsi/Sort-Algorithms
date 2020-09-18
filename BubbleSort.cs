b64a02294061ade6193bf41f5712a033557a16e2
AHorejsi <alex.horejsi59@gmail.com>
2020-09-18 01:21:55 -0700

    Implemented bubble sort and selection sort

---
+++ BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace Sorting {
+    public class BubbleSorter : ComparisonSorter {
+        public override void Sort(IList list, int low, int high, IComparer comparer) {
+            if (low < high) {
+                for (int index1 = low + 1; index1 < high; ++index1) {
+                    bool swapped = false;
+                    int index2 = low;
+                    int index3 = index2 + 1;
+
+                    while (index3 < high) {
+                        if (comparer.Compare(list[index2], list[index3]) > 0) {
+                            SortingUtils.Swap(list, index2, index3);
+                            swapped = true;
+                        }
+
+                        ++index2;
+                        ++index3;
+                    }
+
+                    if (!swapped) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace Sorting {
+    public class SelectionSorter : ComparisonSorter {
+        public override void Sort(IList list, int low, int high, IComparer comparer) {
+            for (int index = low; index < high; ++index) {
+                int minIndex = SortingUtils.MinIndex(list, index, high, comparer);
+
+                if (minIndex != index) {
+                    SortingUtils.Swap(list, index, minIndex);
+                }
+            }
+        }
+    }
+}
