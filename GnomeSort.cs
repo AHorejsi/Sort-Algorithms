9e26f0762e325fde9357e45f623200b282ed3d44
AHorejsi <alex.horejsi59@gmail.com>
2020-09-24 19:50:04 -0700

    Implemented Gnome sort

---
+++ GnomeSort.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Sorting {
+    public class GnomeSorter : ComparisonSorter {
+        public override void Sort(IList list, int low, int high, IComparer comparer) {
+            int index = low;
+
+            while (index < high) {
+                if (0 == index) {
+                    ++index;
+                }
+
+                if (comparer.Compare(list[index], list[index - 1]) >= 0) {
+                    ++index;
+                }
+                else {
+                    SortUtils.Swap(list, index, index - 1);
+                    --index;
+                }
+            }
+        }
+    }
+}
