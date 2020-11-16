9a1ac8374269b4706ea22631fdcc9390669547ab
AHorejsi <alex.horejsi59@gmail.com>
2020-11-15 22:27:56 -0800

    Changes

--- Immutable/ImmutableIntegerSort.cs
+++ Immutable/ImmutableIntegerSort.cs
@@ -8,19 +8,5 @@ namespace Sorting.Immutable {
         }
 
         public IImmutableList<T> Sort(IImmutableList<T> list, int low, int high);
-
-        public async Task<IImmutableList<T>> SortAsync(IImmutableList<T> list) {
-            Task<IImmutableList<T>> task = Task.Run(() => this.Sort(list));
-            await task;
-
-            return task.Result;
-        }
-
-        public async Task<IImmutableList<T>> SortAsync(IImmutableList<T> list, int low, int high) {
-            Task<IImmutableList<T>> task = Task.Run(() => this.Sort(list, low, high));
-            await task;
-
-            return task.Result;
-        }
     }
 }
